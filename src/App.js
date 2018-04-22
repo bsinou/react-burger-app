5bf7bd8861469e61682161bacfc42f86e309d030
Bruno SINOU <bruno.sinou@posteo.de>
2018-04-22 21:39:41 +0200

    Polish error management

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
 import Layout from './hoc/Layout/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder' 
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
+
+  // state = {
+  //   show: true
+  // };
+
+  // componentWillMount() {
+  //   setTimeout(()=> {this.setState({show: false})}, 5000);
+  // }
+
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder /> 
+          {/* {this.state.show ? <BurgerBuilder /> : null} */}
+          <BurgerBuilder />
         </Layout>
       </div>
     );
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -3,24 +3,33 @@ import React, { Component } from 'react';
 import Aux from '../Aux/Aux'
 import Modal from '../../components/UI/Modal/Modal'
 
-
-
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
 
-        state = {
+        state = { // enable display and disposal of the popup.
             error: null
         }
 
-        componentDidMount() {
-            axios.interceptors.request.use(req => {
+        // We rather use component*WILL*Mount:
+        // -> the below calls have no side effect (we just register)
+        // -> the error interceptor is registered even if the very first rendering fails,
+        //     e.g. that will means that the componentDidMount method of this level is never called 
+        componentWillMount() {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({ error: null });
                 return req;
             });
 
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({ error: error });
             });
+        }
+
+
+        // Don't forget to dispose (eject) the interceptors to avoid memory leaks
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         disposeModal = () => {
@@ -38,7 +47,6 @@ const withErrorHandler = (WrappedComponent, axios) => {
                     <WrappedComponent {...this.props} />
                 </Aux>
             );
-
         }
     }
 }
